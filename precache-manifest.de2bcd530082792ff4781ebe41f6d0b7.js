1b6cb4d60b88b9aeac45e09fbfb2771d16d91c8d
v-potap <v.o.potapenko@gmail.com>
2019-09-10 17:40:07 +0300

    Updates

---
+++ precache-manifest.de2bcd530082792ff4781ebe41f6d0b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458c121171fd3a9dfe560438ff62c19a",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "7121ef9845bcaffb645e",
+    "url": "/goit-react-hw-04-reader/static/css/main.97783b70.chunk.css"
+  },
+  {
+    "revision": "d4de8e2755d89bec178c",
+    "url": "/goit-react-hw-04-reader/static/js/2.d9283d19.chunk.js"
+  },
+  {
+    "revision": "7121ef9845bcaffb645e",
+    "url": "/goit-react-hw-04-reader/static/js/main.e5b75e18.chunk.js"
+  },
+  {
+    "revision": "a04e0ac9f148b9ff203f",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.a5c7ec70.js"
+  }
+]);
